c663f3679e07163f01f8535f02bed10153f6aa1f
j <mahamadoun.toure1@gmail.com>
2022-01-07 15:59:30 +0100

    add views

---
+++ app/views/Frontend/home.php
@@ -0,0 +1,8 @@
+<h1>Je suis la page d'accueil</h1>
+
+<ul>
+    <?php /** @var $posts \App\Entity\Post[] */
+    foreach ($posts as $post) : ?>
+        <li><?= $post->getTitle(); ?></li>
+    <?php endforeach; ?>
+</ul>
